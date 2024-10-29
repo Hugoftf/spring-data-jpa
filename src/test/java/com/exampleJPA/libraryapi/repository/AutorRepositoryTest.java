78b2e9742e006b6458f24ca80fa150ac822670ad
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2024-10-29 20:51:49 -0300

    Testing the methods delete by id and delete the object

--- src/test/java/com/exampleJPA/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/com/exampleJPA/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,8 +44,40 @@ public class AutorRepositoryTest {
             autorEncontrado.setNascionalidade("Argentino");
 
             autorRepository.save(autorEncontrado);
+        }
+    }
 
+    @Test
+    public void listarTeste(){
+        List<Autor> listaAutor = autorRepository.findAll();
+        for (Autor autor:listaAutor){
+            System.out.println(autor);
+        }
+    }
 
+    @Test
+    public void contagemTeste(){
+        System.out.println("Contagem de autores: " + autorRepository.count());
+    }
+
+    @Test
+    public void deletarPorIdTeste(){
+        var id = UUID.fromString("c90187b6-9b54-4304-b25b-7b386dfa4f79");
+        if (id != null){
+            autorRepository.deleteById(id);
+        }
+        else{
+            throw new IllegalArgumentException("Id nullo");
+        }
+    }
+
+    @Test
+    public void deleterObjeto(){
+        var id = UUID.fromString("9141cb1f-a417-4d54-b7ac-6b1aa7fdfdf5");
+        var autor = autorRepository.findById(id);
+        if (autor.isPresent()) {
+            var autorEncontrado = autor.get();
+            autorRepository.delete(autorEncontrado);
         }
     }
 }
